4b229b39a58d37de2371c9a71ad521367c4d31b4
Arch23 <noda.94@gmail.com>
2018-05-09 22:49:31 -0300

    adding features

--- app/PillBox.js
+++ app/PillBox.js
@@ -1,16 +1,37 @@
-var add = event => {
-    if (event.keyCode === 13 && event.target.value !== "") {
-        let li = document.createElement("li");
-        li.innerHTML = `${event.target.value} <span>&times</span>`;
-        li.querySelector('span').addEventListener("click", e => {
-            e.target.parentNode.remove();
-        });
-        event.target.value = "";
-        event.target.parentNode.insertBefore(li, event.target);
-    }
+var allOptions = [];
+var selectDefault;
+var selectedOptions = [];
+var select;
+
+var updateSelect = () => {
+    
 };
 
-var init = component => {
+var add = event => {
+    const exists = allOptions.findIndex(el => el.text.toLowerCase().includes(event.target.value.toLowerCase()));
+    if (event.target.value !== '') {
+        if(event.keyCode === 13 && exists != -1){
+            let li = document.createElement("li");
+            li.innerHTML = `${allOptions[exists].text} <span>&times</span>`;
+            li.querySelector('span').addEventListener("click", e => {
+                e.target.parentNode.remove();
+            });
+            event.target.value = "";
+            event.target.parentNode.insertBefore(li, event.target);
+            selectedOptions.push(allOptions[exists].text);
+            updateSelect();
+        }else{
+            while(select.firstChild){
+                select.removeChild(select.firstChild);
+            }
+            allOptions.filter(el=>el.text.toLowerCase().includes(event.target.value.toLowerCase())).forEach(el => select.appendChild(el));
+            return;
+        }
+    }
+    select.innerHTML = selectDefault;
+};
+
+var init = () => {
     let ul = document.createElement("ul");
     ul.classList += "pillbox";
     ul.innerHTML =
@@ -19,16 +40,29 @@ var init = component => {
     let input = ul.querySelector(".pillbox-input");
 
     input.resize();
-    input.addEventListener("keypress", add);
+    input.addEventListener("keyup", add);
+    /* input.addEventListener('focus',(e) => {
+        select.style.display = 'block';
+    })
+    input.addEventListener('focusout',e => {
+        select.style.display = 'none';
+    }); */
 
     ul.addEventListener("click", e => {
         ul.querySelector(".pillbox-input").focus();
     });
 
-    component.before(ul);
-    component.style.display = "none";
-    };
+    select.before(ul);
+    ul.appendChild(select);
+    select.classList += "pillbox-options";
 
-    HTMLSelectElement.prototype.PillBox = function() {
-    init(this);
+    for(let i=0;i<select.length;i++){
+        allOptions.push(select.options[i]);
+    }
+    selectDefault = select.innerHTML;
+};
+
+HTMLSelectElement.prototype.PillBox = function() {
+    select = this;
+    init();
 };
